ce8abfa2e689de9f4a7e024f25728af32bfa898c
Jim McGaw <smoochy@Jim-McGaws-MacBook-Pro.local>
2013-07-23 19:01:57 -0700

    first commit

---
+++ spec/web.spec.js
@@ -0,0 +1,11 @@
+describe("#sayGreeting", function() {
+	it("says the greeting with the name", function() {
+		expect("this").toEqual("that");
+	});
+});
+
+describe("A suite", function() {
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
+});
---
+++ web.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var _ = require('underscore');
+var fs = require("fs");
+
+
+var app = express();
+
+app.use("/scripts", express.static(__dirname + '/public/scripts'));
+app.use("/images", express.static(__dirname + '/public/images'));
+
+app.get('/', function(request, response) {
+  response.sendfile("index.html");
+});
+
+app.get('/capture', function(request, response){
+  
+});
+
+var port = process.env.PORT || 5000;
+
+var crypto = require('crypto'),
+      https = require("https");
+
+var privateKey = fs.readFileSync('privatekey.pem').toString();
+var certificate = fs.readFileSync('certificate.pem').toString();
+var options = {key: privateKey, cert: certificate};
+
+var server = https.createServer(options, app);
+server.listen(port);
+console.log("Listening on port " + port);
+
+var sayGreeting = function(greeting, name) {
+	return greeting + ", " + name;
+};
