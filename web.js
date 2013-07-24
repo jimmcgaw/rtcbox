3c033667a309f77d46d0da068df16e5a03309795
Jim McGaw <smoochy@Jim-McGaws-MacBook-Pro.local>
2013-07-23 19:59:22 -0700

    code to boot server

--- web.js
+++ web.js
@@ -19,11 +19,26 @@ app.get('/capture', function(request, response){
 var port = process.env.PORT || 5000;
 
 var crypto = require('crypto'),
-      http = require("http");
+      http = require("http"),
+      https = require("https");
 
-var server = http.createServer(app);
+var privateKey = fs.readFileSync('server.key').toString();
+var certificate = fs.readFileSync('server.crt').toString();
+var options = {key: privateKey, cert: certificate};
+
+var server;
+
+if (process.env.NODE_ENV === "production"){
+	// Heroku production environment
+	server = http.createServer(app);
+} else {
+	// local dev
+	server = https.createServer(options, app);
+}
 server.listen(port);
 console.log("Listening on port " + port);
+
+
 
 var sayGreeting = function(greeting, name) {
 	return greeting + ", " + name;
